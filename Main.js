71705a7418c7d7a11a74709fe58ac24bb41347d2
Augustine Edeh <info.augustinesedeh@gmail.com>
2021-06-16 09:55:04 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,97 @@
+//var he = document.getElementsByClassName('_operators')[0].getElementsByClassName("me")[0];
+//  he.style = "color: blue;";
+//  alert(he.innerHTML);
+
+
+
+
+
+
+
+
+
+
+var _0 = document.getElementById("_0").innerHTML;
+var _1 = document.getElementById("_1").innerHTML;
+var _2 = document.getElementById("_2").innerHTML;
+var _3 = document.getElementById("_3").innerHTML;
+var _4 = document.getElementById("_4").innerHTML;
+var _5 = document.getElementById("_5").innerHTML;
+var _6 = document.getElementById("_6").innerHTML;
+var _7 = document.getElementById("_7").innerHTML;
+var _8 = document.getElementById("_8").innerHTML;
+var _9 = document.getElementById("_9").innerHTML;
+
+
+
+
+//var _
+
+
+
+
+var answer = document.getElementById("_answer");
+
+function wrt_0() {
+  answer.value = _0;
+}
+
+
+
+function wrt_1() {
+  answer.value = _1;
+}
+
+
+
+function wrt_2() {
+  answer.value = _2;
+}
+
+
+
+function wrt_3() {
+  answer.value = _3;
+}
+
+
+
+function wrt_4() {
+  answer.value = _4;
+}
+
+
+
+function wrt_5() {
+  answer.value = _5;
+}
+
+
+
+function wrt_6() {
+  answer.value = _6;
+}
+
+
+
+function wrt_7() {
+  answer.value = _7;
+}
+
+
+
+
+function wrt_8() {
+  answer.value = _8;
+}
+
+
+
+function wrt_9() {
+  answer.value = _9;
+}
+
+
+
+
+//alert(_7);
